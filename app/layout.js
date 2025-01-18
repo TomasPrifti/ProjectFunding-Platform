8c12f5081b515dd32267db8ea1afcd6e986b0629
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-18 20:00:56 +0100

    feat(layout): aggiunto componente `Head` e `favicon`

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,20 @@
 'use client'
 
+import Head from "next/head";
 import { MoralisProvider } from "react-moralis";
 import "../styles/globals.css";
+
 import Header from '@/components/header';
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<Head>
+				<title>ProjectFunding</title>
+				<meta name="description" content="ProjectFunding - Decentralized Crowdfunding Platform" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
 			<body>
 				<MoralisProvider initializeOnMount={false}>
 					<Header />
