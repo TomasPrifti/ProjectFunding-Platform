eb6cab73dff7316d91dba404afc5bfaea0f2d9af
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-20 19:58:53 +0100

    feat(newProject): creazione nuova pagina con campi per la creazione di un nuovo progetto + gestione file di stile

--- app/newProject/page.js
+++ app/newProject/page.js
@@ -1,8 +1,45 @@
+'use client'
+
+import { contractAddresses, abi } from '@/constants/index';
+
 const NewProject = () => {
+
+	const createProject = () => {
+		return;
+	}
+
 	return (
-		<div>
-			<h1>Create a New Project</h1>
-			<p>Test for NewProject</p>
+		<div className="new-project">
+			<h1>Create Your New Project</h1>
+
+			<div className="fields">
+				<div className="field field-name">
+					<label htmlFor="name">Name</label>
+					<input type="text" id="name" name="name" />
+				</div>
+
+				<div className="field field-description">
+					<label htmlFor="description">Description</label>
+					<textarea id="description" name="description" rows="3" cols="35" />
+				</div>
+
+				<div className="field field-expiration">
+					<label htmlFor="expiration">Expiration Time(specify days)</label>
+					<input type="number" id="expiration" name="expiration" step="1" min="1" />
+				</div>
+
+				<div className="field field-goal">
+					<label htmlFor="goal">Goal (specify USDT)</label>
+					<input type="number" id="goal" name="goal" step="1" min="1" />
+				</div>
+
+				<div className="field field-min-capital">
+					<label htmlFor="min-capital">Minimum Capital to Invest(specify USDT)</label>
+					<input type="number" id="min-capital" name="min-capital" step="1" min="1" />
+				</div>
+			</div>
+
+			<button onClick={createProject}>Create Project</button>
 		</div>
 	);
 }
