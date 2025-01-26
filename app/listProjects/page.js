d70b22d2e194a62e151da8e2a832e7cd4b24ce1a
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-26 19:56:57 +0100

    feat(list-project): gestione pagina `listProjects` con elenco dei progetti con componente `ProjectCard`

--- app/listProjects/page.js
+++ app/listProjects/page.js
@@ -1,8 +1,85 @@
+"use client"
+
+import { ethers } from "ethers";
+import { useContext, useEffect, useState } from "react";
+import { contractAddresses, abi } from "@/constants/index";
+import { UserContext } from "@/utils/context";
+import ProjectCard from "@/components/project-card";
+
 const ListProjects = () => {
+	const {
+		user,
+		setUser
+	} = useContext(UserContext);
+
+	const [projects, setProjects] = useState([]);
+
+	const getAllProjects = async () => {
+		if (typeof window.ethereum === "undefined") {
+			console.error("MetaMask doesn't exist.");
+			return;
+		}
+
+		let provider;
+
+		try {
+			provider = new ethers.BrowserProvider(window.ethereum);
+
+			// Check if the contract Manager is deployed.
+			const contractCode = await provider.getCode(contractAddresses[user.chainId]["Manager"]);
+			if (contractCode === "0x") {
+				console.error("Error: The main contract doesn't exist");
+				return;
+			}
+		} catch (error) {
+			console.error("Error in wallet connection:", error);
+			return;
+		}
+		// Retrieve the contract Manager already deployed.
+		const manager = new ethers.Contract(contractAddresses[user.chainId]["Manager"], abi[user.chainId]["Manager"], provider)
+		const allProjectsAddresses = await manager.getAllProjects();
+		const tempProjects = [];
+
+		for (const address of allProjectsAddresses) {
+			const project = new ethers.Contract(address, abi[user.chainId]["Project"], provider)
+
+			const obj = {
+				address: project.target,
+				name: await project.getName(),
+				description: await project.getDescription(),
+				expiration: await project.getExpiration(),
+				goal: await project.getGoal(),
+				minCapital: await project.getMinCapital(),
+				targetWallet: await project.getTargetWallet(),
+				status: await project.getStatus(),
+				currentBalance: await project.getUSDTBalance(),
+				myCapitalInvested: await project.getMyCapitalInvested(),
+			};
+
+			tempProjects.push(obj);
+		}
+
+		setProjects(tempProjects);
+	}
+
+	useEffect(() => {
+		if (!user?.chainId) {
+			return;
+		}
+		getAllProjects();
+	}, [user]);
+
 	return (
-		<div>
+		<div className="list-projects">
 			<h1>All Projects</h1>
-			<p>Test for AllProjects</p>
+
+			<div className="grid-projects">
+				{projects.map((project) => {
+					return (
+						<ProjectCard key={project.address} project={project} />
+					);
+				})}
+			</div>
 		</div>
 	);
 }
