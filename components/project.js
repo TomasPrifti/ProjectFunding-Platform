74e15d043f1a430ca140a050e8c4772059d26517
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-26 22:58:20 +0100

    feat(project): gestione diverse tipologie di `view` + regole di stile

--- components/project.js
+++ components/project.js
@@ -1,28 +1,52 @@
 import { formatUnits } from "ethers";
 import Link from "next/link";
 
-const Project = ({ project, view }) => {
+const Project = ({ project, view = "card" }) => {
 	const hrefData = {
 		pathname: '/getProject',
 		query: { address: project.address },
 	};
 
 	return (
-		<Link href={hrefData} className="project">
-			<h2 className="name">{project.name}</h2>
-			<p className="description">{project.description}</p>
+		<>
+			{view === "card" && (
+				<Link href={hrefData} className="project view-card">
+					<h2 className="name">{project.name}</h2>
+					<p className="description">{project.description}</p>
 
-			<div className="details">
-				<p><span>Address:</span> {project.address.slice(0, 6)}...{project.address.slice(-4)}</p>
-				<p><span>Expiration time:</span> {new Date(parseInt(project.expiration) * 1000).toLocaleDateString()}</p>
-				<p><span>Goal:</span> {formatUnits(project.goal, 6)} USDT</p>
-				<p><span>Minimum capital to invest:</span> {formatUnits(project.minCapital, 6)} USDT</p>
-				<p><span>Current capital invested:</span> {formatUnits(project.currentBalance, 6)} USDT</p>
-				<p><span>How much I've invested:</span> {formatUnits(project.myCapitalInvested, 6)} USDT</p>
-				<p><span>Target wallet:</span> {project.targetWallet.slice(0, 6)}...{project.targetWallet.slice(-4)}</p>
-				<p><span>Status:</span> {project.status}</p>
-			</div>
-		</Link>
+					<div className="details">
+						<p><span>Expiration time:</span> {new Date(parseInt(project.expiration) * 1000).toLocaleDateString()}</p>
+						<p><span>Goal:</span> {formatUnits(project.goal, 6)} USDT</p>
+						<p><span>Minimum capital to invest:</span> {formatUnits(project.minCapital, 6)} USDT</p>
+						<p><span>Current capital invested:</span> {formatUnits(project.currentBalance, 6)} USDT</p>
+						<p><span>Status:</span> {project.status}</p>
+					</div>
+				</Link>
+			)}
+			{view === "full" && (
+				<div className="project view-full">
+					<h1 className="name">{project.name}</h1>
+					<p className="description">{project.description}</p>
+
+					<div className="details">
+						<div>
+							<p><span>Expiration time:</span> {new Date(parseInt(project.expiration) * 1000).toLocaleDateString()}</p>
+							<p><span>Goal:</span> {formatUnits(project.goal, 6)} USDT</p>
+							<p><span>Minimum capital to invest:</span> {formatUnits(project.minCapital, 6)} USDT</p>
+						</div>
+
+						<div>
+							<p><span>Address:</span> {project.address}</p>
+							<p><span>Target wallet:</span> {project.targetWallet}</p>
+						</div>
+
+						<p><span>Current capital invested:</span> {formatUnits(project.currentBalance, 6)} USDT</p>
+
+						<p><span>Status:</span> {project.status}</p>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
