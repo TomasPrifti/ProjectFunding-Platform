cf875fdc0bc14947fdb7d986769e7166c15070c6
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-26 21:40:25 +0100

    feat(get-project): creazione pagina per la gestione del singolo `Project` + regole di stile

---
+++ app/getProject/page.js
@@ -0,0 +1,73 @@
+"use client"
+
+import { ethers } from "ethers";
+import { useContext, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { abi } from "@/constants/index";
+import { UserContext } from "@/utils/context";
+import ProjectCard from "@/components/project-card";
+
+const GetProject = () => {
+	const {
+		user,
+		setUser
+	} = useContext(UserContext);
+
+	const searchParams = useSearchParams();
+	const [project, setProject] = useState();
+
+	const getProjectData = async (address) => {
+		if (typeof window.ethereum === "undefined") {
+			console.error("MetaMask doesn't exist.");
+			return;
+		}
+
+		let signer;
+
+		try {
+			const provider = new ethers.BrowserProvider(window.ethereum);
+			signer = await provider.getSigner();
+		} catch (error) {
+			console.error("Error in wallet connection:", error);
+			return;
+		}
+		// Retrieve the contract Project already deployed.
+		const project = new ethers.Contract(address, abi[user.chainId]["Project"], signer)
+
+		const obj = {
+			address: project.target,
+			name: await project.getName(),
+			description: await project.getDescription(),
+			expiration: await project.getExpiration(),
+			goal: await project.getGoal(),
+			minCapital: await project.getMinCapital(),
+			targetWallet: await project.getTargetWallet(),
+			status: await project.getStatus(),
+			currentBalance: await project.getUSDTBalance(),
+			myCapitalInvested: await project.getMyCapitalInvested(),
+		};
+
+		setProject(obj);
+	}
+
+	useEffect(() => {
+		const address = searchParams.get('address');
+		if (!user?.chainId || !address) {
+			return;
+		}
+
+		getProjectData(address);
+	}, [user]);
+
+	return (
+		<div className="get-project">
+			{project ? (
+				<ProjectCard key={project.address} project={project} />
+			) : (
+				<h1>This project doesn't exist !</h1>
+			)}
+		</div>
+	);
+}
+
+export default GetProject;
