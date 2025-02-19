0bafead9e92b9bf4dade44607a3861c0698e1d81
Tomas Prifti <tomasprifti99@gmail.com>
2025-02-19 20:29:32 +0100

    feat(constants): aggiunta costante per la gestione del link per `etherscan`

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,13 @@
 const contractAddresses = require('./contractAddresses.json');
 const abi = require('./abi.json');
+const etherscanLink = {
+	1: "https://etherscan.io/", // Mainnet.
+	11155111: "https://sepolia.etherscan.io/", // Sepolia Testnet.
+	31337: "", // Hardhat Local.
+}
 
 module.exports = {
 	contractAddresses,
 	abi,
+	etherscanLink,
 }
